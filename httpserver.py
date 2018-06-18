ae246d21cd372250655271f76783ec1a17f72cc5
alejoloaiza <alejo.loaiza@gmail.com>
2018-06-17 21:52:55 -0500

    [Add] Added a route

--- httpserver.py
+++ httpserver.py
@@ -1,22 +1,11 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from server import Datos
 app = Flask(__name__)
 
-recommendations = [
-    {
-        'id': 3,
-        'uuid': 'xyz123',
-        'username': 'fmauricios',
-        'name': u'Mauricio Serna',
-    },
-    {
-        'id': 4,
-        'uuid': 'xyz123444',
-        'username': 'arpo',
-        'name': u'Ana Restrepo',
-    }
-]
 
-@app.route('/api/recommendations', methods=['GET'])
-def get_users():
-    return jsonify({'recommendations': recommendations})
+@app.route('/api/recommendations', methods=['POST'])
+def get_recommendation():
+    if request.method == 'POST':
+        req_data = request.get_json()
+        print(req_data)
+        return jsonify({'recommendations': req_data})
